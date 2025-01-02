40264780e488112b6476e1da66d1339b99b5bf43
KHY90 <power4206@gmail.com>
2025-01-02 23:00:19 +0900

    OOD연습

---
+++ main.py
@@ -0,0 +1,35 @@
+class Dog:
+
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+class GuardDog(Dog): 
+
+    def rrr(self):
+        print("Stay away!")
+
+class Puppy(Dog):
+
+    def __str__(self):
+        return f"{self.name}, {self.breed}"
+
+    def woof_woof(self):
+        print("woof")
+
+    def introduce(self):
+        self.woof_woof()
+        print(f"My name is {self.name} and i am a {self.breed}")
+
+ruffus = Puppy(
+    name = "Ruffus",
+    breed = "Beagel"
+    )
+
+bibi = Puppy(
+    name = "bibi",
+    breed = "dalmatian"
+    )
+
+ruffus.introduce()
---
+++ practice.py
@@ -0,0 +1,74 @@
+# 숫자
+print(5)
+print(-5)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*3+1)
+# 문자형
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋ")
+print("ㅋ"*9)
+# boolean
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not (5 > 10))
+# 애완동물을 소개해 주세요~
+animal = "강아지"
+name = "키티"
+age = 6
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집 " + animal + "이름은 " +  name + " 예요.")
+print(name + "는 " + str(age) + "살 이구요" + hobby + "를 좋아해요.")
+print(name + "는 어른일까요? " + str(is_adult))
+
+# 퀴즈
+
+station = "사당"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "신도림"
+print(station + "행 열차가 들어오고 있습니다.")
+station = "인천공항"
+print(station + "행 열차가 들어오고 있습니다.")
+
+# 연산자
+print(1+1)
+print(2-1)
+print(6*4)
+print(10/2)
+
+print(10**2) # 10^2 = 100
+print(5%3) # 나머지 2 
+print(10%3) # 1
+print(5//3)  # 목연산 1
+print(10//3)  # 목연산 3
+
+print(10 > 3)  # True
+print(4 >= 7)  # False
+print(10 < 3)  # False
+print(5 <= 5)  # True
+
+print(3 == 3)  # True
+print(4 == 3)  # False
+print(3 + 4 == 7)  # True
+
+print(1 != 3)  # True
+print(not(1 != 3))  # False
+
+print((3 > 0) and (3 < 5))  # True
+print((3 > 0) & (3 < 5))  # True
+
+print((3 > 0) or (3 < 5))  # True
+print((3 > 0) | (3 < 5))  # True
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
+
+
